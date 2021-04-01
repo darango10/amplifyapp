36d01a2ec29bba18c157c4a3f2d048fc15c87628
Daniel Arango <arangomonsalve.daniel@gmail.com>
2021-04-01 12:13:50 -0500

    Form lambda

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './512px-Amazon_Web_Services_Logo.svg.png';
-import './App.css';
+import Formulario from "./Formulario";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Let´s build an AWS hosted app {'💻'}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Formulario/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Formulario.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import {Button, Form} from "react-bootstrap";
+import axios from "axios";
+
+const Formulario = () => {
+
+    const [nombre, setNombre] = useState({
+        firstName: '',
+        lastName: ''
+    });
+
+    const [respuesta, setRespuesta] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('https://j95sdr1uch.execute-api.us-east-1.amazonaws.com/dev/', nombre).then((res) => {
+            setRespuesta(res.data.body)
+        })
+    }
+
+
+    return (
+        <>
+            <Form className="container container-fluid mt-5" onSubmit={handleSubmit}>
+                <Form.Group controlId="formBasicFirstName">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter First Name" value={nombre.firstName}
+                                  onChange={e => setNombre({...nombre, firstName: e.target.value})}/>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicLastName">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Last Name" value={nombre.lastName}
+                                  onChange={e => setNombre({...nombre, lastName: e.target.value})}/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+
+            {respuesta !== null ? <p>{respuesta}</p> : null}
+        </>
+    );
+};
+
+export default Formulario;
